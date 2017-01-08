d5d0cf4182675d44d2e3f07c96047e2b1698d1b3
“Nicolay Hekkens <mailto:nicolay.hekkens@purestone.co.uk>
2017-01-08 02:52:55 +0000

    adjustments 3

--- tank.js
+++ tank.js
@@ -1,96 +1,99 @@
 // wiring-pi
 var wpi = require('wiring-pi');
-var forwardPin = 10;
-var leftPin = 11;
-var backPin = 12;
-var rightPin = 13;
+// var forwardPin = 10;
+// var leftPin = 11;
+// var backPin = 12;
+// var rightPin = 13;
+// wpi.setup('wpi');
+// wpi.pinMode(forwardPin, wpi.OUTPUT);
+// wpi.pinMode(leftPin, wpi.OUTPUT);
+// wpi.pinMode(backPin, wpi.OUTPUT);
+// wpi.pinMode(rightPin, wpi.OUTPUT);
+
+// // socket.io
+// var app = require('http').createServer(handler)
+// var io = require('socket.io')(app);
+// var fs = require('fs');
+
+// app.listen(1337);
+
+// function handler (req, res) {
+//   fs.readFile(__dirname + '/app/views/index.html',
+//   function (err, data) {
+//     if (err) {
+//       res.writeHead(500);
+//       return res.end('Error loading index.html');
+//     }
+
+//     res.writeHead(200);
+//     res.end(data);
+//   });
+// }
+
+// io.on('connection', function (socket) {
+//   socket.on('command', function (data) {
+//     console.log('Command Received: ', data);
+
+//     // forward
+//     if( data.forward ) {
+//     	console.log('forward - ON');
+//     	wpi.digitalWrite(forwardPin, 1 );
+//     } else {
+//     	console.log('forward - OFF');
+//     	wpi.digitalWrite(forwardPin, 0 );
+//     }
+
+//     // left
+//     if( data.left ) {
+//     	console.log('left - ON');
+//     	wpi.digitalWrite(leftPin, 1 );
+//     } else {
+//     	console.log('left - OFF');
+//     	wpi.digitalWrite(leftPin, 0 );
+//     }
+
+//     // back
+//     if( data.back ) {
+//     	console.log('back - ON');
+//     	wpi.digitalWrite(backPin, 1 );
+//     } else {
+//     	console.log('back - OFF');
+//     	wpi.digitalWrite(backPin, 0 );
+//     }
+
+//     // right
+//     if( data.right ) {
+//     	console.log('right - ON');
+//     	wpi.digitalWrite(rightPin, 1 );
+//     } else {
+//     	console.log('right - OFF');
+//     	wpi.digitalWrite(rightPin, 0 );
+//     }
+
+//     console.log('Command Executed');
+//   });
+// });
+
+
+
+// "wiring-pi": ">=2.1.1",
+// GPIO pin of the led
+
+var configPin = 7;
+// Blinking interval in usec
+var configTimeout = 1000;
+
 wpi.setup('wpi');
-wpi.pinMode(forwardPin, wpi.OUTPUT);
-wpi.pinMode(leftPin, wpi.OUTPUT);
-wpi.pinMode(backPin, wpi.OUTPUT);
-wpi.pinMode(rightPin, wpi.OUTPUT);
+wpi.pinMode(configPin, wpi.OUTPUT);
 
-// socket.io
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
-var fs = require('fs');
+var isLedOn = 0;
 
-app.listen(1337);
+setInterval(function() {
+	isLedOn = +!isLedOn;
+	//isLedOn = !isLedOn;
+	wpi.digitalWrite(configPin, isLedOn );
+	console.log(configPin, isLedOn);
+}, configTimeout);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/app/views/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
-  socket.on('command', function (data) {
-    console.log('Command Received: ', data);
-
-    // forward
-    if( data.forward ) {
-    	console.log('forward - ON');
-    	wpi.digitalWrite(forwardPin, 1 );
-    } else {
-    	console.log('forward - OFF');
-    	wpi.digitalWrite(forwardPin, 0 );
-    }
-
-    // left
-    if( data.left ) {
-    	console.log('left - ON');
-    	wpi.digitalWrite(leftPin, 1 );
-    } else {
-    	console.log('left - OFF');
-    	wpi.digitalWrite(leftPin, 0 );
-    }
-
-    // back
-    if( data.back ) {
-    	console.log('back - ON');
-    	wpi.digitalWrite(backPin, 1 );
-    } else {
-    	console.log('back - OFF');
-    	wpi.digitalWrite(backPin, 0 );
-    }
-
-    // right
-    if( data.right ) {
-    	console.log('right - ON');
-    	wpi.digitalWrite(rightPin, 1 );
-    } else {
-    	console.log('right - OFF');
-    	wpi.digitalWrite(rightPin, 0 );
-    }
-
-    console.log('Command Executed');
-  });
-});
 
 console.log('Server - Running');
-
-
-// // "wiring-pi": ">=2.1.1",
-// // GPIO pin of the led
-
-// var configPin = 7;
-// // Blinking interval in usec
-// var configTimeout = 1000;
-
-// wpi.setup('wpi');
-// wpi.pinMode(configPin, wpi.OUTPUT);
-
-// var isLedOn = 0;
-
-// setInterval(function() {
-// 	isLedOn = +!isLedOn;
-// 	//isLedOn = !isLedOn;
-// 	wpi.digitalWrite(configPin, isLedOn );
-// }, configTimeout);
