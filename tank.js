c1e52fb2c2cb36c13436f24b4c624de873979cc9
“Nicolay Hekkens <mailto:nicolay.hekkens@purestone.co.uk>
2017-01-08 02:06:09 +0000

    initial led test

--- tank.js
+++ tank.js
@@ -1,5 +1,15 @@
 // wiring-pi
-// var wpi = require('wiring-pi');
+var wpi = require('wiring-pi');
+var forwardPin = 10;
+var leftPin = 11;
+var backPin = 12;
+var rightPin = 13;
+wpi.setup('wpi');
+wpi.pinMode(forwardPin, wpi.OUTPUT);
+wpi.pinMode(leftPin, wpi.OUTPUT);
+wpi.pinMode(backPin, wpi.OUTPUT);
+wpi.pinMode(rightPin, wpi.OUTPUT);
+
 // socket.io
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
@@ -21,17 +31,46 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
   socket.on('command', function (data) {
-    console.log(data);
+    console.log('Command Received: ', data);
+
+    // forward
+    if( data.forward ) {
+    	wpi.digitalWrite(forwardPin, wpi.HIGH );
+    } else {
+    	wpi.digitalWrite(forwardPin, wpi.LOW );
+    }
+
+    // left
+    if( data.left ) {
+    	wpi.digitalWrite(leftPin, wpi.HIGH );
+    } else {
+    	wpi.digitalWrite(leftPin, wpi.LOW );
+    }
+
+    // back
+    if( data.back ) {
+    	wpi.digitalWrite(backPin, wpi.HIGH );
+    } else {
+    	wpi.digitalWrite(backPin, wpi.LOW );
+    }
+
+    // right
+    if( data.right ) {
+    	wpi.digitalWrite(rightPin, wpi.HIGH );
+    } else {
+    	wpi.digitalWrite(rightPin, wpi.LOW );
+    }
+
+    console.log('Command Executed');
   });
 });
 
 console.log('Server - Running');
 
 
-// "wiring-pi": ">=2.1.1",
-// GPIO pin of the led
+// // "wiring-pi": ">=2.1.1",
+// // GPIO pin of the led
 
 // var configPin = 7;
 // // Blinking interval in usec
